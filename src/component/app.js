feebca9147af4de2836562d0eb8bc745d96b4785
xgs <727471025@qq.com>
2017-09-10 20:52:19 +0800

    test12

--- src/component/app.js
+++ src/component/app.js
@@ -2,6 +2,7 @@ import React from 'react'
 import style from './app.local.less'
 console.log('App.js exec!!!!!!!!!')
 const test11 = 11
+const test12 = 12
 export default class App extends React.Component {
   render() {
     return (
